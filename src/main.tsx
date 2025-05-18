7a47adcc2973319495a4762d09d289377fb60d7e
Surtling <surtling44@protonmail.com>
2025-05-18 23:32:34 +1000

    Fix browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+const basename = import.meta.env.BASE_URL; // Vite injects this from the `base` config
+
 import App from './App.tsx'
 import './style.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
